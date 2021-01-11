fbde0c35fbf4c47695dc691f1372c9d1c3cdf54b
Emmanuel Delobre <emmanueldelobre@MacBook-Pro-Avril-2019.local>
2021-01-11 14:46:47 +0100

    Section8 Hibernate entites liees 42 Les DTO (Data Transfert Object)

--- src/main/java/com/mycompany/tennis/controller/EpreuveController.java
+++ src/main/java/com/mycompany/tennis/controller/EpreuveController.java
@@ -2,7 +2,8 @@ package com.mycompany.tennis.controller;
 
 import java.util.Scanner;
 
-import com.mycompany.tennis.core.entity.Epreuve;
+import com.mycompany.tennis.core.dto.EpreuveFullDto;
+import com.mycompany.tennis.core.dto.EpreuveLightDto;
 import com.mycompany.tennis.core.service.EpreuveService;
 
 public class EpreuveController {
@@ -17,7 +18,7 @@ public class EpreuveController {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Quel est le nom de l'epreuve dont vous voulez afficher les informations");
 		long identifiant = scanner.nextLong();
-		Epreuve epreuve = epreuveService.getEpreuveAvecTournoi(identifiant);
+		EpreuveFullDto epreuve = epreuveService.getEpreuveAvecTournoi(identifiant);
 		System.out.println("Le nom  du tournoi est "+epreuve.getTournoi().getNom());
 		
 	}
@@ -26,7 +27,7 @@ public class EpreuveController {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Quel est le nom de l'epreuve dont vous voulez afficher les informations");
 		long identifiant = scanner.nextLong();
-		Epreuve epreuve = epreuveService.getEpreuveSansTournoi(identifiant);
+		EpreuveLightDto epreuve = epreuveService.getEpreuveSansTournoi(identifiant);
 		
 	}
 }
